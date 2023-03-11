d32842714502904f46ab0b00ed3bf35cfd555ca8
Lalitha <lalithagehlot99@gmail.com>
2023-03-11 12:22:34 +0530

    addition

--- 1.java
+++ 1.java
@@ -3,5 +3,7 @@ import java.util.*;
 class Solution {
     public static void main(String args[]) {
         System.out.println("Hii");
+        int a = 10, b = 20;
+        System.out.println(a + b);
     }
 }
